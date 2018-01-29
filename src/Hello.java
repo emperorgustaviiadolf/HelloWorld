acf46b6a8bd17ed815e760dcf3e1d7a1d070241e
Unknown <19syverj@elmbrookstudents.org>
2018-01-29 11:22:00 -0600

    Updated city, state, and street address

--- src/Hello.java
+++ src/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 		System.out.println("Hello World!");
 		System.out.println("Jakob");
 		System.out.println("1585 Hickory Hill Lane");
+		System.out.println("Brookfield, Wisconsin, 53045");
 	}
 }
