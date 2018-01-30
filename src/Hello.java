4e4fb37b49c390d15b9756d3e8403d843f4e9a90
Unknown <19syverj@elmbrookstudents.org>
2018-01-30 10:11:12 -0600

    Added Kawaii Function

--- src/Hello.java
+++ src/Hello.java
@@ -6,5 +6,6 @@ public class Hello {
 		System.out.println("1585 Hickory Hill Lane");
 		System.out.println("Brookfield, Wisconsin, 53045");
 		System.out.println("Syke that's not my real address, idiot!");
+		System.out.println("I-I-It's not like I liek you... Baka! :3");
 	}
 }
